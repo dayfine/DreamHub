7f3b150a61260d5fcbb4508c62718a9af395cd19
Burcu Noyan <burcunoyan@gmail.com>
2017-11-08 21:33:19 -0500

    associate Goal to User; create new Goal in seed

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,26 @@
 const conn = require('./conn')
-const Users = require('./Users')
+const User = require('./Users')
+const Goal = require('./Goal')
+
+Goal.belongsTo(User);
+User.hasMany(Goal);
 
 const sync = () => conn.sync({ force: true })
 const seed = () => {
-  Users.create({ name: 'John' })
+  Promise.all([
+    User.create({ name: 'John' }),
+    Goal.create({ title: 'Travel', description: 'Want to go to Burundi' })
+  ])
+  .then(([ u1, g1 ]) => {
+    u1.addGoal(g1)
+  })
 }
 
 module.exports = {
   sync,
   seed,
-  models: { Users }
+  models: {
+    User,
+    Goal
+  }
 }
