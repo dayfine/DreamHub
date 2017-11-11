15ecade2f599640bc7ac5ed904550a5a02c1b443
Burcu Noyan <burcunoyan@gmail.com>
2017-11-10 21:14:00 -0500

    fix typo

--- server/api/goals.js
+++ server/api/goals.js
@@ -1,27 +1,27 @@
 const router = require('express').Router();
-const { Task } = require('../db').models;
+const { Goal } = require('../db').models;
 
 router.get('/', (req, res, next) => {
-  Goals.getGoals(req.body.userId)
+  Goal.getGoals(req.body.userId)
     .then(goals => res.send(goals))
     .catch(next);
   });
 });
 
 router.post('/', (req, res, next) => {
-  Goals.addGoal(req.body.userId, req.body)
+  Goal.addGoal(req.body.userId, req.body)
     .then(goal => res.send(goal))
     .catch(next);
 });
 
 router.put('/:id', (req, res, next) => {
-  Goals.editGoal(req.params.id, req.body.userId, req.body)
+  Goal.editGoal(req.params.id, req.body.userId, req.body)
     .then(goal => res.send(goal))
     .catch(next);
 });
 
 router.delete('/:id', (req, res, next) => {
-  Goals.deleteGoal(req.params.id, req.body.userId)
+  Goal.deleteGoal(req.params.id, req.body.userId)
     .then(() => res.sendStatus(201))
     .catch(next);
 });
