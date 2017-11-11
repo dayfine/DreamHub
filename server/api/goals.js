9c727b26b5b07c1876384c2104286e88f1d28b05
Burcu Noyan <burcunoyan@gmail.com>
2017-11-10 21:08:12 -0500

    add initial Goals routes

---
+++ server/api/goals.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const { Goal, User } = require('../db').models;
+
+router.get('/', (req, res, next) => {
+  Goals.getGoals(req.body.userId)
+    .then(goals => res.send(goals))
+    .catch(next);
+  });
+});
+
+router.post('/', (req, res, next) => {
+  Goals.addGoal(req.body.userId, req.body)
+    .then(goal => res.send(goal))
+    .catch(next);
+});
+
+router.put('/:id', (req, res, next) => {
+  Goals.editGoal(req.params.id, req.body.userId, req.body)
+    .then(goal => res.send(goal))
+    .catch(next);
+});
+
+router.delete('/:id', (req, res, next) => {
+  Goals.deleteGoal(req.params.id, req.body.userId)
+    .then(() => res.sendStatus(201))
+    .catch(next);
+});
+
+module.exports = router;
