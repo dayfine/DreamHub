d755f513e60a16e4fc6ef0debaceb4aceca9161f
Burcu Noyan <burcunoyan@gmail.com>
2017-11-14 19:59:48 -0500

    add getGoals thunk

--- client/src/Goals/actions.js
+++ client/src/Goals/actions.js
@@ -1,6 +1,13 @@
 import { ADD_GOAL, UPDATE_GOAL, DELETE_GOAL, FETCH_GOALS } from './actionTypes.js'
+import axios from 'axios';
 
 const addGoal = newGoal => ({ type: ADD_GOAL, newGoal })
 const updateGoal = updatedGoal => ({ type: UPDATE_GOAL, updatedGoal })
 const deleteGoal = id => ({ type: DELETE_GOAL, id })
 const fetchGoals = goals => ({ type: FETCH_GOALS, goals })
+
+export const getGoals = () => dispatch => {
+  axios.get('/api/goals')
+    .then(res => res.data)
+    .then(goals => dispatch(fetchGoals(goals)))
+}
