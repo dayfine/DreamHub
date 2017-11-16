e7317631e6fd84215ca693db34193769aaae5b0c
Burcu Noyan <burcunoyan@gmail.com>
2017-11-15 20:51:10 -0500

    can edit goal

--- client/src/Goals/views/GoalForm.jsx
+++ client/src/Goals/views/GoalForm.jsx
@@ -9,25 +9,41 @@ class Form extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
-    // this.handleEdit = this.handleEdit.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleSave = this.handleSave.bind(this);
   }
 
   handleChange(ev) {
     this.setState({ value: ev.target.value });
   }
 
+  // handles adding new goal
   handleSubmit(ev) {
     ev.preventDefault();
     this.props.createGoal(1, this.state.value); // userId hardcoded
+    this.setState({ value: '' });
   }
 
   handleDelete(id) {
     this.props.removeGoal(id * 1);
   }
 
+  // handles editing goal
+  handleEdit(ev) {
+    const currentGoal = Object.assign({}, this.state.currentGoal, { title: ev.target.value });
+    this.setState({ currentGoal });
+  }
+
+  // handles saving edited goal
+  handleSave(ev) {
+    ev.preventDefault();
+    this.props.editGoal(this.state.currentGoal, 1); // userId hardcoded
+    this.setState({ showForm: false });
+  }
+
   render() {
     const { value, showForm } = this.state;
-    const { handleSubmit, handleChange, handleDelete, handleEdit } = this;
+    const { handleSubmit, handleChange, handleDelete, handleEdit, handleSave } = this;
     const { goals } = this.props;
 
     return (
@@ -36,40 +52,30 @@ class Form extends Component {
           <input type="text" value={ value } onChange={ handleChange } placeholder="Add new goal..." className="goal-input" />
           <button type="submit" className="btn btn-sm btn-primary">+</button>
         </form>
-        <div>
-          <ul>
-            {
-              goals.map(goal => {
-                return (
-                  <li key={ goal.id } className="goal-item"><span className="goal-title">{ goal.title }</span>
-                    <button onClick={() =>  this.setState({ showForm: true, currentGoal: goal })} className="btn btn-sm btn-secondary">Edit</button> <button onClick={() => handleDelete(`${ goal.id }`)} className="btn btn-sm btn-danger">x</button>
-                  </li>
-                )
-              })
-            }
-          </ul>
+        <ul>
           {
-            !showForm ? null : <EditGoalForm { ...this.props } { ...this.state } />
+            goals.map(goal => {
+              return (
+                <li key={ goal.id } className="goal-item">
+                  {
+                    !showForm ? (
+                        <span onClick={() =>  this.setState({ showForm: true, currentGoal: goal })}  className="goal-title">{ goal.title }</span>
+                      ) : (
+                        <form onSubmit={ handleSave } className="goal-edit">
+                          <input type="text" onChange={ handleEdit } value={ this.state.currentGoal.title } className="goal-input-sm" /> <button className="btn btn-sm btn-success">Save</button>
+                        </form>
+                      )
+                  }
+                  <button onClick={() => handleDelete(`${ goal.id }`)} className="btn btn-sm btn-danger">x</button>
+                </li>
+              )
+            })
           }
-        </div>
+        </ul>
       </div>
     )
   }
 }
-
-const EditGoalForm = ({ value, currentGoal, editGoal }) => {
-  const handleEdit = (ev) => {
-    ev.preventDefault();
-    editGoal(currentGoal, ev.target.value); // fix this
-  }
-
-  return (
-    <form onSubmit={ handleEdit }>
-      <input type="text" value={ value } />
-      <button className="btn btn-sm btn-primary">Save</button>
-    </form>
-  )
-};
 
 const mapStateToProps = ({ goals }) => {
   return { goals };
