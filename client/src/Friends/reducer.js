fe295f6085d04267af44bfa7f0e97da81c800aa2
Jerry Hsiung <jhsiung91@gmail.com>
2017-11-16 15:36:11 -0500

    created friends actions,actiontype,reducer

---
+++ client/src/Friends/reducer.js
@@ -0,0 +1,29 @@
+import { ADD_FRIEND, UPDATE_FRIEND, DELETE_FRIEND, FETCH_FRIENDS } from './actionTypes.js'
+
+const initialState = [
+
+]
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_FRIEND:
+      return [ ...state, action.newFriend ]
+
+    case UPDATE_FRIEND:
+      const lnIdx = state.findIndex(elem => elem.id === action.friendId)
+      const nextState = state.slice()
+      const updatedFriend = Object.assign({}, state[lnIdx], action.changes)
+
+      nextState.splice(lnIdx, 1, updatedFriend)
+      return nextState
+
+    case DELETE_FRIEND:
+      return state.filter(elem => elem.id !== action.friendId)
+
+    case FETCH_FRIENDS:
+      return action.friends
+
+    default:
+      return state
+  }
+}
