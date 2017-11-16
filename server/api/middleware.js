b68de1d59e4c553665b8dc8c46f304948e547fd5
Burcu Noyan <burcunoyan@gmail.com>
2017-11-15 21:20:16 -0500

    comment out unused middleware for now

--- server/api/middleware.js
+++ server/api/middleware.js
@@ -1,13 +1,13 @@
-const { User } = require('../db').models;
+// const { User } = require('../db').models;
 
-const findUser = (req, res, next) => {
-  if (!userId) res.sendStatus(401);
+// const findUser = (req, res, next) => {
+//   if (!userId) res.sendStatus(401);
 
-  User.findById(req.body.userId)
-    .then(user => {
-      if (!user) return res.sendStatus(401);
-      next();
-    });
-};
+//   User.findById(req.body.userId)
+//     .then(user => {
+//       if (!user) return res.sendStatus(401);
+//       next();
+//     });
+// };
 
-module.exports = findUser;
+// module.exports = findUser;
