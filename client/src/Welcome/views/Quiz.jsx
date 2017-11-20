7490a13ea42c1c19ed98f628fee7dfd0b8e1ed34
panda <neptmanhall@gmail.com>
2017-11-20 18:58:25 -0500

    html slider added

--- client/src/Welcome/views/Quiz.jsx
+++ client/src/Welcome/views/Quiz.jsx
@@ -4,9 +4,10 @@ import List, { ListItem, ListItemText } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
 import Button from 'material-ui/Button';
 import Input from 'material-ui/Input';
+//import Slider from 'material-ui/Slider';
+//import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-const Quiz = ()=> {
-  const questionStyle = {
+const questionStyle = {
     width: `45vw`,
     height: `40vh`,
     marginRight: 'auto',
@@ -48,19 +49,38 @@ const Quiz = ()=> {
     marginRight: 'auto'
   }
   
+  const sliderStyle = {
+    marginLeft: '10vw',
+    marginRight: '4vw',
+    marginTop: '8vh',
+    width: `60%`
+  }
+  
+class Quiz extends React.Component{
+  constructor(){
+    super();
+    this.state ={
+      sliderNum: 5
+    }
+  }
+  
+  render(){
+    let { sliderNum } = this.state;
   return (
-    <Grid container style={questionStyle}>
-      <List>
-        <h3 style={titleStyle}>If your name was a color, what color would it be?</h3>
-        <Divider />
-        <p style={bodyStyle}>Write a goal you have. Be specific.</p>
-        <Input style={inputStyle}/>
-      </List>
-        <Button size='small' color='accent' style={buttonStyle}>Skip</Button>
-        <Button size='small' color='primary' style={buttonStyle}>Next</Button>
-      
-    </Grid>
+      <Grid container style={questionStyle}>
+        <List>
+          <h3 style={titleStyle}>If your name was a color, what color would it be?</h3>
+          <Divider />
+          <p style={bodyStyle}>Write a goal you have. Be specific.</p>
+          <Input style={inputStyle}/>
+          <input onChange={(event)=> this.setState({sliderNum: event.target.value })} type="range" min="1" max="10" value={sliderNum} style={sliderStyle} />
+        </List>
+          <Button size='small' color='accent' style={buttonStyle}>Skip</Button>
+          <Button size='small' color='primary' style={buttonStyle}>Next</Button>
+
+      </Grid>
   )
+  }
 } 
 
 export default Quiz;
