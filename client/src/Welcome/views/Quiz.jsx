a28f80f1924dc636f332889c297cad7a2a95ac9d
panda <neptmanhall@gmail.com>
2017-11-21 10:56:16 -0500

    adding state to quiz component

--- client/src/Welcome/views/Quiz.jsx
+++ client/src/Welcome/views/Quiz.jsx
@@ -6,80 +6,50 @@ import Button from 'material-ui/Button';
 import Input from 'material-ui/Input';
 //import Slider from 'material-ui/Slider';
 //import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+//import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import styles from './styles';
 
-const questionStyle = {
-    width: `45vw`,
-    height: `40vh`,
-    marginRight: 'auto',
-    marginLeft: 'auto',
-    borderRadius: `10px`,
-    boxShadow: `3px 4px 10px grey`,
-  }
-  
-  const titleStyle = {
-    fontFamily: 'sans-serif',
-    fontSize: `3vh`,
-    width: '40vw',
-    textAlign: 'center',
-    marginRight: 'auto',
-    marginLeft: 'auto',
-    padding: `.3vw`
-  }
-  
-  const bodyStyle = {
-    padding: '1vw',
-    textAlign: 'center',
-    marginRight: 'auto',
-    marginLeft: 'auto'
-  }
-  
-  const inputStyle ={
-    width: `60%`,
-    marginRight: '4vw',
-    marginLeft: '10vw'
-  }
-  
-  const buttonStyle = {
-    outline: 'none',
-    width: `50%`,
-  }
-  
-  const textareaStyles = {
-    marginLeft: 'auto',
-    marginRight: 'auto'
-  }
-  
-  const sliderStyle = {
-    marginLeft: '10vw',
-    marginRight: '4vw',
-    marginTop: '8vh',
-    width: `60%`
-  }
-  
 class Quiz extends React.Component{
   constructor(){
     super();
     this.state ={
-      sliderNum: 5
+      sliderNum: 1,
+      goal: [],
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  handleChange(event){
+    //to be removed
+    this.setState({ goal: [...this.state.goal, event.target.value] })
+  }
+  
+  handleSubmit(event){
+    event.preventDefault();
+    event.target.myInput.value='';
   }
   
   render(){
-    let { sliderNum } = this.state;
-  return (
-      <Grid container style={questionStyle}>
-        <List>
-          <h3 style={titleStyle}>If your name was a color, what color would it be?</h3>
-          <Divider />
-          <p style={bodyStyle}>Write a goal you have. Be specific.</p>
-          <Input style={inputStyle}/>
-          <input onChange={(event)=> this.setState({sliderNum: event.target.value })} type="range" min="1" max="10" value={sliderNum} style={sliderStyle} />
-        </List>
-          <Button size='small' color='accent' style={buttonStyle}>Skip</Button>
-          <Button size='small' color='primary' style={buttonStyle}>Next</Button>
+    const { sliderNum, goal } = this.state;
+    const { questionStyle, titleStyle, bodyStyle, inputStyle, buttonStyle, textareaStyle, sliderStyle } = styles;
+    const { handleSubmit, handleChange } = this;
+    return (
+        <Grid container style={questionStyle}>
+          <form onSubmit={handleSubmit}>
+            <h3 style={titleStyle}>If your name was a color, what color would it be?</h3>
+            <Divider />
+            <p style={bodyStyle}></p>
+            <Input style={inputStyle} onChange={handleChange} name="myInput" placeholder="Write a goal you have. Be specific."/>
+            <input label="How important is this?" onChange={(event)=> this.setState({sliderNum: event.target.value })} type="range" min="1" max="10" value={sliderNum} style={sliderStyle} />
+            <p style={bodyStyle}>{sliderNum}</p>
+            <p style={bodyStyle}>Your loser goal: {goal}</p>
+            <Button size='small' color='accent' style={buttonStyle}>Skip</Button>
+            <Button size='small' color='primary' style={buttonStyle}>Next</Button>
+          </form>
 
-      </Grid>
-  )
+        </Grid>
+    )
   }
 } 
 
---
+++ client/src/Welcome/views/styles.js
@@ -0,0 +1,52 @@
+const styles = {
+  questionStyle: {
+      width: `45vw`,
+      height: `60vh`,
+      marginRight: 'auto',
+      marginLeft: 'auto',
+      borderRadius: `10px`,
+      boxShadow: `3px 4px 10px grey`,
+    },
+
+  titleStyle: {
+      fontFamily: 'sans-serif',
+      fontSize: `3vh`,
+      width: '40vw',
+      textAlign: 'center',
+      marginRight: 'auto',
+      marginLeft: 'auto',
+      padding: `.3vw`
+    },
+
+  bodyStyle: {
+      padding: '1vw',
+      textAlign: 'center',
+      marginRight: 'auto',
+      marginLeft: 'auto'
+    },
+
+  inputStyle: {
+      width: `60%`,
+      marginRight: '4vw',
+      marginLeft: '10vw'
+    },
+
+  buttonStyle: {
+      outline: 'none',
+      width: `50%`,
+    },
+
+  textareaStyle: {
+      marginLeft: 'auto',
+      marginRight: 'auto'
+    },
+
+  sliderStyle: {
+      marginLeft: '10vw',
+      marginRight: '4vw',
+      marginTop: '8vh',
+      width: `60%`
+    }
+}
+
+export default styles;
