a4ef544deaf6e218e6f085c9895646fe148bb6eb
dayfine <difancpa@gmail.com>
2017-11-23 15:09:03 -0500

    toggle task status on single goal view

--- client/src/Goals/views/GoalPanel.jsx
+++ client/src/Goals/views/GoalPanel.jsx
@@ -3,38 +3,61 @@ import { connect } from 'react-redux'
 
 import Paper from 'material-ui/Paper'
 import Divider from 'material-ui/Divider'
+import { withStyles } from 'material-ui/styles'
 import Typography from 'material-ui/Typography'
+import green from 'material-ui/colors/green'
+import Checkbox from 'material-ui/Checkbox'
+
+import { TASK_STATUS } from '../../constants'
+import { editTask } from '../../Tasks/actions'
+
+const styles = {
+  checked: {
+    color: green[500]
+  }
+}
 
 const GoalPanel = props => {
-  const { goal } = props
-  console.log(goal)
+  const { goal, goalTasks, classes, editTask } = props
+  const { COMPLETED, CREATED } = TASK_STATUS
+
+  const toggleStatus = (taskId) => {
+    const task = goalTasks.find(t => t.id === taskId)
+    const newStatus = task.status === COMPLETED ? CREATED : COMPLETED
+    editTask({...task, status: newStatus})
+  }
 
   return (
     <Paper>
       <Typography type='headline' >
-        {goal.title}
+        { goal.title }
       </Typography>
       <Typography type='display1' >
-        {goal.description}
+        { goal.description }
       </Typography>
       <Divider light />
       <ol>
-      {goal.tasks && goal.tasks.map(task => {
-        return (
-          <li key={task.id}>
-            Title: {task.title}
-            <br />
-            Description: {task.description}
-            <br />
-            Due Date: {task.dueDate}
-            <br />
-            Priority: {task.priority}
-            <br />
-            Status: {task.status}
-            <Divider light />
-          </li>
-        )
-      })}
+        { goalTasks.map(task => {
+          return (
+            <li key={task.id}>
+              Title: { task.title }
+              <br />
+              Description: { task.description }
+              <br />
+              Due Date: { task.dueDate }
+              <br />
+              Priority: { task.priority }
+              <br />
+              Status: { task.status }
+              <Divider light />
+              <Checkbox
+                checked={task.status === COMPLETED}
+                onChange={toggleStatus.bind(this, task.id)}
+                className={classes.checked}
+              />
+            </li>
+          )
+        })}
       </ol>
     </Paper>
   )
@@ -44,10 +67,14 @@ const mapState = (state, ownProps) => {
   const { goalId } = ownProps.match.params
   console.log(goalId)
   return {
-    goal: state.goals.find(g => g.id === +goalId)
+    goal: state.goals.find(g => g.id === +goalId),
+    goalTasks: state.tasks.filter(t => t.goalId === +goalId)
   }
 }
 
-const mapDispatch = ({ })
+const mapDispatch = ({ editTask })
 
-export default connect(mapState, mapDispatch)(GoalPanel)
+export default connect(mapState, mapDispatch)(
+                withStyles(styles)(
+                  GoalPanel
+                ))
