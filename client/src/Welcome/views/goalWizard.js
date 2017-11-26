965083e46ac20b43cb90ba1a72b3294e92329964
panda <neptmanhall@gmail.com>
2017-11-26 16:02:01 -0500

    removed temp array in place of goalsWizard object

---
+++ client/src/Welcome/views/goalWizard.js
@@ -0,0 +1,10 @@
+//Make human a variable for the current user
+const goalWizard = [{
+  id: 0,
+  title: 'Welcome human, do you have a new goal?',
+  description: 'hello'
+
+  }
+]
+
+export default goalWizard;
