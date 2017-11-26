3639221a901c0b4a4e4215b5a4df9657aaaace68
Burcu Noyan <burcunoyan@gmail.com>
2017-11-26 00:56:30 -0500

    remove SET_LOCATION action

--- client/src/Travel/reducer.js
+++ client/src/Travel/reducer.js
@@ -1,10 +1,7 @@
-import { SET_LOCATION, GET_FLIGHT } from './actionTypes';
+import { GET_FLIGHT } from './actionTypes';
 
 export default (state = '', action) => {
   switch(action.type) {
-    case SET_LOCATION:
-      return action.location || '';
-
     case GET_FLIGHT:
       return action.flight;
 
