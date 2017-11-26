7a0341b1864dcb813873c9d2d2fbc4ed18d0421e
panda <neptmanhall@gmail.com>
2017-11-26 16:02:01 -0500

    removed temp array in place of goalsWizard object

--- client/src/Welcome/views/Quiz.jsx
+++ client/src/Welcome/views/Quiz.jsx
@@ -8,8 +8,9 @@ import Input from 'material-ui/Input';
 //import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 //import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import styles from './styles';
-import { createGoal } from '../actions'
-import { connect } from 'react-redux'
+import { createGoal } from '../actions';
+import { connect } from 'react-redux';
+import goalWizard from './goalWizard';
 
 
 class Quiz extends React.Component {
@@ -39,30 +40,25 @@ class Quiz extends React.Component {
     const { sliderNum, goal, currentUser } = this.state;
     const { questionStyle, titleStyle, bodyStyle, inputStyle, buttonStyle, textareaStyle, sliderStyle } = styles;
     const { handleSubmit, handleChange } = this;
-
-    const tempQuestions = ['Welcome Message', 'Do you have a new goal?', 'Is your deadline realistic?' ];
-    //Welcome {currentUser}, do you have a new goal?
     return (
         <Grid container >
 
            {
-              tempQuestions.map(question => {
+
+              goalWizard.map(question => {
                  return (
-                   <form style={questionStyle} onSubmit={handleSubmit}>
-                      <h3 style={titleStyle}>{question}</h3>
+                   <form style={questionStyle} onSubmit={handleSubmit} key={question.id}>
+                      <h3 style={titleStyle}>{question.title}</h3>
                       <Divider />
                       <p style={bodyStyle}></p>
                       <Input style={inputStyle} onChange={handleChange} name="myInput" placeholder="Be as specific as possible." autoFocus/>
                       <input label="How important is this?" onChange={(event)=> this.setState({sliderNum: event.target.value })} type="range" min="1" max="10" value={sliderNum} style={sliderStyle} />
                       <p style={bodyStyle}>{sliderNum}</p>
                       <Button size='small' color='accent' style={buttonStyle}>Skip</Button>
-                      <Button size='small' color='primary' style={buttonStyle} onClick={()=> this.setState({counter: this.state.counter+1})}>Next {this.state.counter}</Button>
+                      <Button size='small' color='primary' style={buttonStyle} onClick={()=> this.setState({counter: this.state.counter+1})}>Next ( {this.state.counter} )</Button>
                   </form>
                 )
-              }).filter((question, i) => {
-                console.log('I IS THE FILTER', question, i)
-                return this.state.counter === i
-              })
+              }).filter((question, i) => this.state.counter === i)
             }
 
       </Grid>
