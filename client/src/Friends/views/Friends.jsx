6aaf950d6bf366c1b4e6da9ecd8ff7c609e85274
dayfine <difancpa@gmail.com>
2017-11-27 11:28:41 -0500

    cleaned up route

--- client/src/Friends/views/Friends.jsx
+++ client/src/Friends/views/Friends.jsx
@@ -1,48 +1,38 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { addFriend, updateFriend, deleteFriend, fetchFriends, findFriend, removeFriend } from '../actions';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addFriend, updateFriend, deleteFriend, fetchFriends, findFriend, removeFriend } from '../actions'
 
-import Divider from 'material-ui/Divider';
+import Divider from 'material-ui/Divider'
 
 class Friend extends Component {
-  constructor (props) {
-    super (props);
-    this.state = {
-      newFriend: {},
-      search: ''
-    }
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
+  state = {
+    newFriend: {},
+    search: ''
   }
 
-  // handles search friend email
-  handleSubmit(ev) {
-    ev.preventDefault();
-    console.log('searchfor '+this.state.search)
-    console.log('THESE ARE THE PROPS:', this.props);
+  handleSubmit = ev => {
+    ev.preventDefault()
     this.props.findFriend(this.state.search)
     // .then(res => console.log(res))
     // this.setState({ newFriend: {} });
   }
-  handleChange(ev) {
+  handleChange = ev => {
     console.log(ev.target.value)
-    this.setState({ search: ev.target.value });
+    this.setState({ search: ev.target.value })
   }
-  handleDelete(id) {
-    console.log('hereeeeee'+id)
-    this.props.removeFriend(id);
+  handleDelete = id => {
+    this.props.removeFriend(id)
   }
-  render() {
-    const { newFriend, search } = this.state;
-    const { friends } = this.props;
-    const { handleSubmit, handleChange, handleDelete} = this;
+  render () {
+    const { newFriend, search } = this.state
+    const { friends } = this.props
+    const { handleSubmit, handleChange, handleDelete} = this
 
     return (
-      <div className="container">
-        <form onSubmit={ handleSubmit }>
+      <div className='container'>
+        <form onSubmit={handleSubmit}>
           Add Friend:
-          <input type="text" placeholder='Search friend by email' onChange={ handleChange } value={ search }/>
+          <input type='text' placeholder='Search friend by email' onChange={handleChange} value={search} />
           <button>Submit</button>
         </form>
         <br />
@@ -51,26 +41,26 @@ class Friend extends Component {
         </div>
 
         <ul>
-        {friends.map(friend => {
-          return (
-            <li key={friend.id}>
-              <h2>{friend.name}</h2>
-              <h3>{friend.email}</h3>
-              {friend.goals.map(goal => {
-                return (
-                  <div key={goal.id}>
-                    <h4>{goal.title}</h4>
-                    <h5>>>> {goal.description}</h5>
-                  </div>
-                )
-              })}
-              <button
+          {friends.map(friend => {
+            return (
+              <li key={friend.id}>
+                <h2>{friend.name}</h2>
+                <h3>{friend.email}</h3>
+                {friend.goals.map(goal => {
+                  return (
+                    <div key={goal.id}>
+                      <h4>{goal.title}</h4>
+                      <h5>>>> {goal.description}</h5>
+                    </div>
+                  )
+                })}
+                <button
                   onClick={handleDelete.bind(this, friend.id)}
-                  className="btn btn-sm btn-danger">Delete Friend</button>
-              <Divider light />
-            </li>
-          )
-        })}
+                  className='btn btn-sm btn-danger'>Delete Friend</button>
+                <Divider light />
+              </li>
+            )
+          })}
         </ul>
       </div>
     )
@@ -80,6 +70,6 @@ class Friend extends Component {
 const mapStateToProps = state => ({
   friends: state.friends
 })
-const mapDispatch = { addFriend, updateFriend, deleteFriend, fetchFriends, findFriend, removeFriend };
+const mapDispatch = { addFriend, updateFriend, deleteFriend, fetchFriends, findFriend, removeFriend }
 
-export default connect(mapStateToProps, mapDispatch)(Friend);
+export default connect(mapStateToProps, mapDispatch)(Friend)
--- server/api/friends.js
+++ server/api/friends.js
@@ -3,85 +3,48 @@ const { User } = require('../db').models
 const jwt = require('jsonwebtoken')
 const secret = process.env.SECRET
 
-// /api/friends
+router
+  .use('/', (req, res, next) => {
+    const token = req['headers'].authorization.split(' ')[1]
+    jwt.verify(token, secret, function (err, decoded) {
+      if (err) return next(err)
 
-router.use('/', (req, res, next) => {
-  const token = req['headers'].authorization.split(' ')[1]
-  jwt.verify(token, secret, function (err, decoded) {
-  	if (err) return next(err)
-  	console.log('no error')
-  	return User.findById(decoded.id)
-	  	.then(user => {
-	  		req.user = user
-	  		next()
-	  	})
+      return User.findById(decoded.id)
+        .then(user => {
+          req.user = user
+          next()
+        })
+    })
   })
-})
 
-// get all friends
-router.get('/', (req, res, next) => {
-  console.log('im in /friends')
-  return User.getFriends() // where friendId = logged in id
-		.then(users => {
-  res.send(users)
-})
-		.catch(next)
-})
-
-// get all friends from logged in user
-// router.get('/friends', (req, res, next) => {
-	// if (req.session.userId) {
-		// User.findOne({
-		// 	where: { userId: req.session.userId },
-		// 	include: {
-		// 		model: User,
-		// 		include: UserFriend
-		// 	}
-		// })
-		// 	.then(friends => {
-		// 		res.send(friends);
-		// 	})
-		// 	.catch(next)
-	// }
-	// else {
-	// 	res.send('not logged in');
-	// }
-// })
-// get all friends
-
-// add new friend
-router.post('/:friendId', (req, res, next) => {
-  return User.findById(req.params.id)
-	.then(newFriend => {
-  User.addFriends(newFriend)// where friendId = logged in id
-  res.send(newFriend)
-})
-	.catch(next)
-})
-
-// search user by email
-router.get('/email/:email', (req, res, next) => {
-  return User.findOne({
-    where: { email: req.params.email}
+  // When to use this one?
+  .get('/', (req, res, next) => {
+    return req.user.getFriends()
+      .then(friends => res.send(friends))
+      .catch(next)
   })
-	.then(friend => {
-  if (friend) {
-			// console.log('routeeeeee ' + friend)
-    return User.addFriends(friend)
-			// .then(()=> res.redirect('/'));
-  }		else {
-    return res.send('user not found')
-  }
-})
-	.catch(next)
-})
 
-router.delete('/:friendId', (req, res, next) => {
-  console.log('got it')
-  return User.findById(req.params.friendId)
-    .then(friend => req.user.removeFriend(friend))
-    .then(() => res.sendStatus(202))
-    .catch(next)
-})
+  .get('/email/:email', (req, res, next) => {
+    return User.findOne({
+      where: { email: req.params.email },
+      attributes: { exclude: ['password'] }
+    })
+      .then(friend => res.send(friend))
+      .catch(next)
+  })
+
+  .post('/:friendId', (req, res, next) => {
+    return User.findById(req.params.id)
+      .then(friend => req.user.addFriend(friend))
+      .then(() => res.sendStatus(202))
+      .catch(next)
+  })
+
+  .delete('/:friendId', (req, res, next) => {
+    return User.findById(req.params.friendId)
+      .then(friend => req.user.removeFriend(friend))
+      .then(() => res.sendStatus(202))
+      .catch(next)
+  })
 
 module.exports = router
