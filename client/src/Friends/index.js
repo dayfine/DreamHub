101ad4e7eb49dc40d5fabb413c0e80f18c31050d
dayfine <difancpa@gmail.com>
2017-11-27 21:37:13 -0500

    separate out friend search login

--- client/src/Friends/index.js
+++ client/src/Friends/index.js
@@ -1,6 +1,6 @@
 import * as actions from './actions'
 import reducer from './reducer'
-import views from './views/Friends'
+import views from './views/FriendList'
 
 export { actions, reducer, views }
-export * from './actions';
+export * from './actions'
