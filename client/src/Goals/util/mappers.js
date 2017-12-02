18bc2b71463f3e2b96561d381f5c971378d1a05a
dayfine <difancpa@gmail.com>
2017-12-02 10:02:00 -0500

    starting goalmapper

--- client/src/Goals/util/mappers.js
+++ client/src/Goals/util/mappers.js
@@ -1,3 +1,5 @@
+import { updateGoal } from '../actions'
+
 export const mapCategoryToGoal = (categories, goals) => {
   let cat
   goals.forEach(g => {
@@ -10,5 +12,19 @@ export const mapCategoryToGoal = (categories, goals) => {
 
 // Update Goal
 export const MapTaksToGoals = (task, goals) => {
+  return goals
+}
+
+// Map tasks to goal while after for status
+export const goalProgressMapper = (goals, tasks) => {
+  goals.forEach(g => {
+    let lastUpdated, completed = true
+    for (let task of tasks) {
+      if (task.goalId === g.id) {
+
+      }
+    }
+  })
+
   return goals
 }
