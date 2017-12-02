2e223c50b09cc489e38b8cd1fa2185cbafd52ccf
Burcu Noyan <burcunoyan@gmail.com>
2017-12-01 20:48:14 -0500

    make presentational component

--- client/src/Graph/views/Graph.js
+++ client/src/Graph/views/Graph.js
@@ -1,31 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { actions } from '../../Tasks';
+import BubbleChart from './BubbleChart';
 
-class Graph extends Component {
-  constructor(props) {
-    super(props);
-    // this.state = { tasks:  }
-  }
+const Graph = (props) => {
+  return (
+    <div id="chart">
+      <h2>Visualize Your Goals</h2>
+      <BubbleChart { ...props } />
+    </div>
+  )
+};
 
-  render() {
-    console.log(this.props)
+const mapState = (props) => { return props };
 
-    return (
-      <div id="chart">
-        <h2>Visualize</h2>
-        <svg width="100" height="100">
-
-        </svg>
-      </div>
-    )
-  }
-}
-
-const mapState = ({ tasks }) => {
-  return { tasks }
-}
-
-const mapDispatch = { actions };
-
-export default connect(mapState, mapDispatch)(Graph);
+export default connect(mapState)(Graph);
