f67bb188a2608aa0018e438f55fb2649f0720670
Burcu Noyan <burcunoyan@gmail.com>
2017-12-02 16:04:42 -0500

    change d3 color scheme

--- client/src/Graph/views/BubbleChart.js
+++ client/src/Graph/views/BubbleChart.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import Bubbles from './Bubbles';
 
 const BubbleChart = ({ goalTasks }) => {
-  const width = 600, height = 800;
+  const width = 600, height = 600;
 
   return (
-    <div>
+    <div id="bubblechart">
       <h2>Visualize Your Goals</h2>
       <Bubbles tasks={goalTasks} width={ width } height={ height } />
     </div>
